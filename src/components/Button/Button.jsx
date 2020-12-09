a91702d7f5a5be9351f489c2187d2ee54f87003c
MacBook <alinauroze@hotmail.com>
2020-12-10 00:49:20 +0500

    feat: props validation added

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Button.css';
 
 function Button({
-  value = '',
-  onClick = null,
-  style = {},
-  disabled = false,
-  className = '',
+  value,
+  onClick,
+  style,
+  disabled,
+  className,
 }) {
   return (
     <button
       type="button"
-      onClick={onClick || null}
+      onClick={onClick}
       className={`button ${className}`}
       style={style}
       disabled={disabled}
@@ -20,5 +21,20 @@ function Button({
     </button>
   );
 }
+
+Button.propType = {
+  onClick: PropTypes.func,
+  className: PropTypes.string,
+  style: PropTypes.shape(PropTypes.any),
+  disabled: PropTypes.bool,
+  value: PropTypes.string.isRequired,
+};
+
+Button.defaultProps = {
+  onClick: null,
+  className: '',
+  style: {},
+  disables: false,
+};
 
 export default Button;

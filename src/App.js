bea28b14a518303ad2193709aa889929684ce133
MacBook <alinauroze@hotmail.com>
2020-12-09 23:00:55 +0500

    feat: viewer added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './res/colors.css';
 import './App.css';
+import Viewer from './components/Viewer/Viewer';
 
 function App() {
   return (
     <>
       Zepto Ai Test
+      <Viewer
+        toView={[
+          { value: 'A', key: 'a' },
+          { value: 'B', key: 'b' },
+        ]}
+        data={[
+          { a: 'Ice', b: 'Snow' },
+          { a: 'Cold', b: 'PCS' },
+        ]}
+      />
     </>
   );
 }
---
+++ src/components/Viewer/Viewer.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import uuid from 'react-uuid';
+
+function Viewer({
+  toView = [],
+  data = [],
+  tableStyle = {},
+}) {
+  return (
+    <table className="table" style={tableStyle}>
+      <thead>
+        {
+            toView.map((heading) => <th key={uuid()}>{heading.value}</th>)
+        }
+      </thead>
+      <tbody>
+        {
+            data.map((row) => (
+              <tr key={uuid()}>
+                {
+                    toView.map((heading) => <td key={uuid()}>{row[heading.key]}</td>)
+                }
+              </tr>
+            ))
+        }
+      </tbody>
+    </table>
+  );
+}
+
+export default Viewer;

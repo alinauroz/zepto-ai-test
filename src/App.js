dbf23d7c5b2f8782f937b67bd3a20babd4852e80
MacBook <alinauroze@hotmail.com>
2020-12-11 13:16:52 +0500

    feat: added Acitvity screen

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
 import React from 'react';
 import './res/colors.css';
 import './App.css';
-import Viewer from './components/Viewer/Viewer';
-import useRequest from './hooks/useRequest';
-import Button from './components/Button/Button';
+
+import Activity from './components/screens/Activity/Activity';
 
 function App() {
-  const { isLoading, response } = useRequest({ route: 'test/people', query: { period: 'day' } });
-
   return (
     <div style={{ width: '95%', marginLeft: '2.5%' }}>
-      <h2>Zepto Ai Test</h2>
-      <Button
-        value="Today"
-        selected
-      />
-      <Button
-        value="This Week"
-      />
-      <br />
-      <br />
-      <Viewer
-        className="people-viewer"
-        toView={[
-          { value: 'Client', key: 'client' },
-          { value: 'Matter', key: 'matter' },
-          { value: 'Description', key: 'description' },
-          { value: 'Type', key: 'type' },
-          { value: 'Time', key: 'time' },
-        ]}
-        data={isLoading ? [] : response.data.people}
-        widths={[13, 13, 48, 13, 13]}
-      />
+      <Activity />
     </div>
   );
 }
---
+++ src/components/screens/Activity/Activity.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Button from '../../Button/Button';
+import Viewer from '../../Viewer/Viewer';
+import useRequest from '../../../hooks/useRequest';
+
+const periods = Object.freeze({
+  day: 'day',
+  week: 'week',
+  month: 'month',
+});
+
+function Activity() {
+  const [period, setPeriod] = React.useState(periods.day);
+  const { isLoading, response } = useRequest({ route: 'test/people', query: { period } });
+
+  return (
+    <>
+      <Button
+        value="Day"
+        onClick={() => setPeriod(periods.day)}
+      />
+      <Button
+        value="This Week"
+        onClick={() => setPeriod(periods.week)}
+      />
+      <Viewer
+        className="people-viewer"
+        toView={[
+          { value: 'Client', key: 'client' },
+          { value: 'Matter', key: 'matter' },
+          { value: 'Description', key: 'description' },
+          { value: 'Type', key: 'type' },
+          { value: 'Time', key: 'time' },
+        ]}
+        data={isLoading ? [] : response.data.people}
+        widths={[13, 13, 48, 13, 13]}
+      />
+    </>
+  );
+}
+
+export default Activity;
